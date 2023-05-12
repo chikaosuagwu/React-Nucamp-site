de82650e4c791967d083d3eb9a5ddbfa7305f0e2
Chika Osuagwu <osuagwuchikau@gmail.com>
2023-05-12 14:47:18 -0700

    Redux campsitesSlice

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -4,24 +4,24 @@ import { selectCommentsByCampsiteId } from "./commentsSlice";
 import CommentForm from "./CommentForm";
 
 const CommentsList = ({ campsiteId }) => {
-    const comments = selectCommentsByCampsiteId(campsiteId);
+  const comments = selectCommentsByCampsiteId(campsiteId);
 
-    if (comments && comments.length > 0) {
-        return (
-            <Col md='5' className="m-1">
-                <h4>Comments</h4>
-                {comments.map(comment => {
-                    return <Comment key={comment.id} comment={comment} />;
-                })}
-                <CommentForm campsiteId={campsiteId} />
-            </Col>
-        )
-    }
+  if (comments && comments.length > 0) {
     return (
-        <Col md='5' className="m-1">
-            There are no comments for this campsite yet.
-        </Col>
-    )
-}
+      <Col md="5" className="m-1">
+        <h4>Comments</h4>
+        {comments.map((comment) => {
+          return <Comment key={comment.id} comment={comment} />;
+        })}
+        <CommentForm campsiteId={campsiteId} />
+      </Col>
+    );
+  }
+  return (
+    <Col md="5" className="m-1">
+      There are no comments for this campsite yet.
+    </Col>
+  );
+};
 
 export default CommentsList;
